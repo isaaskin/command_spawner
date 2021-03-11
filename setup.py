276c22a7815e213fa6d56e9aba60e220980e147c
isa <isasamedaskin@live.com>
2021-03-11 21:44:40 +0100

    Small updates

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
 from setuptools import setup, find_packages
+import pathlib
 
-VERSION = '1.0'
+VERSION = '1.1.1'
 DESCRIPTION = 'Command Spawner'
-LONG_DESCRIPTION = 'Command Spawner is a non-blocking command runner library ' \
-                   'for Python'
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 # Setting up
 setup(
     name="command_spawner",
     version=VERSION,
     author="Isa Askin",
-    author_email="isa33649@gmail.com",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],  # add any additional packages that
     keywords=['python', 'command spawner', 'spawn', 'command', 'shell',
               'bash'],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Education",
-        "Programming Language :: Python :: 2",
+    lassifiers=[
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+        "Operating System :: POSIX :: Linux",
+    ],
 )
